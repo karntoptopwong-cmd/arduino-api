e5d4cbfa9f3088b251cdff5ad0ccccb836dfe3e0
karntoptopwong-cmd <karntoptopwong@gmail.com>
2026-02-14 11:37:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let scores = {};
+
+app.post("/add", (req, res) => {
+  const { user, points } = req.body;
+
+  if (!scores[user]) scores[user] = 0;
+  scores[user] += Number(points);
+
+  res.json({ total: scores[user] });
+});
+
+app.get("/points/:user", (req, res) => {
+  res.json({ points: scores[req.params.user] || 0 });
+});
+
+app.listen(3000, () => console.log("Server running"));
 
