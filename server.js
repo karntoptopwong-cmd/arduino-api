79f4d7cd02d5d21eb9d13b7752e3d178685d25e5
karntoptopwong-cmd <karntoptopwong@gmail.com>
2026-02-14 11:51:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 
-let scores = {};
+let score = 0;
+
+// หน้าแรก
 app.get("/", (req, res) => {
-  res.send("Arduino API running");
+  res.send("API running");
 });
 
-app.post("/add", (req, res) => {
-  const { user, points } = req.body;
-
-  if (!scores[user]) scores[user] = 0;
-  scores[user] += Number(points);
-
-  res.json({ total: scores[user] });
-});
-
-app.get("/points/:user", (req, res) => {
-  res.json({ points: scores[req.params.user] || 0 });
+// เพิ่มคะแนน
+app.get("/add", (req, res) => {
+  score++;
+  res.send("added " + score);
 });
 
 app.listen(3000, () => console.log("Server running"));
-
